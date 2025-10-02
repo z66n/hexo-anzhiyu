fa8bc684939a22d414821f0e96a21d70cb0fdf8d
z66n <47192580+z66n@users.noreply.github.com>
2025-10-02 23:28:28 +0200

    Add new post

--- public/anzhiyu/random.js
+++ public/anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/10/02/hello-world/"];function toRandomPost(){
+var posts=["2025/10/02/hello-world/","2025/10/02/博客文档/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
